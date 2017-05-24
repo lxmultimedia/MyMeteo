5e9ed03ccaa5a91bd2567eb7710669a4306efafb
lxm <lxmediacom@gmail.com>
2017-05-24 21:27:57 +0200

    - added automatic retrieval of IATA code of the next airport

--- app/src/main/java/ch/ffhs/esa/mymeteo/FlugActivity.java
+++ app/src/main/java/ch/ffhs/esa/mymeteo/FlugActivity.java
@@ -1,6 +1,7 @@
 package ch.ffhs.esa.mymeteo;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -12,7 +13,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
+import com.opencsv.CSVReader;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class FlugActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    public String locationFrom = "";
+    public String locationTo = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +54,27 @@ public class FlugActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        setBrowser();
+        getData();
 
     }
 
 
-    private void setBrowser() {
+    private void getData() {
 
-        String locationFrom = "Zürich";
-        String locationTo = "Roma";
+        float radiusDistance = 30;
+        //Zürich
+        float fLat1 = 47.366F;
+        float fLong1 = 8.550F;
 
-        String url = "https://www.google.ch/flights/?gl=ch#search;f=ZRH,ZLP;t=MXP,LIN,BGY,IPR,XIK;";
+        //Paris
+        float fLat2 = 48.864F;
+        float fLong2 = 2.349F;
+
+
+        String codeStart = getAirportCode(radiusDistance, fLong1, fLat1);
+        String codeEnd = getAirportCode(radiusDistance, fLong2, fLat2);
+
+        String url = "https://www.google.ch/flights/?gl=ch#search;f=" + codeStart + ";t=" + codeEnd + ";";
 
 
         WebView webview = (WebView) findViewById(R.id.webcambrowsersbb);
@@ -57,6 +89,81 @@ public class FlugActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
+    private String getAirportCode(float distance, float fLong, float fLat) {
+
+        String urlString = "http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=stations&requestType=retrieve&format=xml&radialDistance="+distance+";"+fLong+","+fLat+"";
+
+        String response = "";
+
+        try {
+            response = new AirportCodeTask().execute(urlString).get().toString();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+
+    private class AirportCodeTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String response = "";
+
+            try {
+                URL url = new URL(params[0]);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(new InputSource(url.openStream()));
+                doc.getDocumentElement().normalize();
+
+                NodeList nodeList = doc.getElementsByTagName("Station");
+
+                for (int i = 0; i < nodeList.getLength(); i++) {
+
+                    Node node = nodeList.item(i);
+
+                    Element fstElmnt = (Element) node;
+                    NodeList nameList = fstElmnt.getElementsByTagName("station_id");
+                    Element nameElement = (Element) nameList.item(0);
+
+                    String iataCode = getIataCode(nameElement.getTextContent().toString());
+                    if(iataCode!="") { iataCode = "," + iataCode; }
+                    response += iataCode;
+
+                }
+            } catch (Exception e) {
+                System.out.println("XML Pasing Excpetion = " + e);
+            }
+
+            if(response.length() > 1) {  response = response.replaceFirst(",","");  }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // execution of result of Long time consuming operation
+        }
+    }
+
+    private String getIataCode(String icaoCode) throws IOException {
+        String iataCode = "";
+
+        CSVReader reader = new CSVReader(new InputStreamReader(getResources().openRawResource(R.raw.icao_iata)), ';');
+
+        String [] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            if (nextLine[1].equals(icaoCode)) {
+                iataCode = nextLine[0];
+                break;
+            }
+        }
+        return iataCode;
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
